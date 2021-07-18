7ffddd8e91f2a91c0612bc7d26a14c7a3255d6ca
blackjack043 <60323672+blackjack043@users.noreply.github.com>
2021-07-18 20:00:34 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,440 @@
+<template>
+    <div class="body">
+
+        <div style="padding:10px">
+            <div class="result-label">Показаны результаты по запросу</div>
+            <div class="result-label-search">Гель-лаки</div>
+        </div>
+        <div >
+<div class="header-block-search" style="display:inline-flex;"> Категории товаров</div>
+<div class="header-block-search" style="display: inline-flex; float: right;"> <img src="hand5.svg"></div>
+        </div>
+        <div id="chip_block" style="width: 150%;position: relative; right:0px" > 
+            
+
+            <div class="chip">
+                Топы 000000000000000
+            </div>
+            <div class="chip">
+                Гель-лак 000000
+            </div>
+            <div class="chip">
+                !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            </div>
+            <div class="chip">
+                Лампы 000000
+            </div>
+            <div class="chip">
+                !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            </div>
+            <div class="chip">
+                Топы 0000000000
+            </div>
+            <div class="chip">
+                !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            </div>
+            <div class="chip">
+                Гель-лак 000000000000000000
+            </div>
+        </div>
+
+        <!--TOVARY-->
+        <div class="header-block-search"> Товары </div>
+
+        <!--ПЕРВАЯ ПОЛОСА-->
+        <div class="row padding" >
+            <div class="col-xs-12 col-md-6 col-lg-4" style="padding-top: 10px;">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-3">
+                            <img class="picture"
+                                src="https://parisnail.ru/upload/resize_cache/iblock/352/162_162_1/2b30cc07_0ff6_11e8_81b9_0cc47a49d0fc_702258c9_5443_11e8_a667_00155d010201.jpg">
+                        </div>
+                        <div class="col-9">
+                            <div class="text">Гель-лак OGP-041 Nightshade ONIQ 10 мл...</div>
+                            <div class="row">
+                                <div class="col-8">
+                                    <div class="text-price" style="display:inline;">1 050P</div>
+                                    <div class="text-price-old" style="display:inline;"> 2 000P </div>
+                                </div>
+
+                                <div class="col-4 svg-cart">
+                                    
+                                    <div class="image"></div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-6 col-lg-4" style="padding-top: 10px;">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-3">
+                            <img class="picture"
+                                src="https://parisnail.ru/upload/resize_cache/iblock/352/162_162_1/2b30cc07_0ff6_11e8_81b9_0cc47a49d0fc_702258c9_5443_11e8_a667_00155d010201.jpg">
+                        </div>
+                        <div class="col-9">
+                            <div class="text">Гель-лак OGP-041 Nightshade ONIQ 10 мл...</div>
+                            <div class="row">
+                                <div class="col-8">
+                                    <div class="text-price" style="display:inline;">1 050P</div>
+                                    <div class="text-price-old" style="display:inline;"> 2 000P </div>
+                                </div>
+
+                                <div class="col-4 svg-cart">
+
+                                    <img src="cart2.svg">
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-6 col-lg-4 " style="padding-top: 10px;">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-3">
+                            <img class="picture"
+                                src="https://parisnail.ru/upload/resize_cache/iblock/352/162_162_1/2b30cc07_0ff6_11e8_81b9_0cc47a49d0fc_702258c9_5443_11e8_a667_00155d010201.jpg">
+                        </div>
+                        <div class="col-9">
+                            <div class="text">Гель-лак OGP-041 Nightshade ONIQ 10 мл...</div>
+                            <div class="row">
+                                <div class="col-8">
+                                    <div class="text-price" style="display:inline;">1 050P</div>
+                                    <div class="text-price-old" style="display:inline;"> 2 000P </div>
+                                </div>
+    
+                                <div class="col-4 svg-cart">
+    
+                                    <img src="cart2.svg">
+    
+                                </div>
+                            </div>
+                        </div>
+    
+                    </div>
+    
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-6 col-lg-4 " style="padding-top: 10px;">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-3">
+                            <img class="picture"
+                                src="https://parisnail.ru/upload/resize_cache/iblock/352/162_162_1/2b30cc07_0ff6_11e8_81b9_0cc47a49d0fc_702258c9_5443_11e8_a667_00155d010201.jpg">
+                        </div>
+                        <div class="col-9">
+                            <div class="text">Гель-лак OGP-041 Nightshade ONIQ 10 мл...</div>
+                            <div class="row">
+                                <div class="col-8">
+                                    <div class="text-price" style="display:inline;">1 050P</div>
+                                    <div class="text-price-old" style="display:inline;"> 2 000P </div>
+                                </div>
+    
+                                <div class="col-4 svg-cart">
+    
+                                    <img src="cart2.svg">
+    
+                                </div>
+                            </div>
+                        </div>
+    
+                    </div>
+    
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-6 col-lg-4 " style="padding-top: 10px;">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-3">
+                            <img class="picture"
+                                src="https://parisnail.ru/upload/resize_cache/iblock/352/162_162_1/2b30cc07_0ff6_11e8_81b9_0cc47a49d0fc_702258c9_5443_11e8_a667_00155d010201.jpg">
+                        </div>
+                        <div class="col-9">
+                            <div class="text">Гель-лак OGP-041 Nightshade ONIQ 10 мл...</div>
+                            <div class="row">
+                                <div class="col-8">
+                                    <div class="text-price" style="display:inline;">1 050P</div>
+                                    <div class="text-price-old" style="display:inline;"> 2 000P </div>
+                                </div>
+    
+                                <div class="col-4 svg-cart">
+    
+                                    <img src="cart2.svg">
+    
+                                </div>
+                            </div>
+                        </div>
+    
+                    </div>
+    
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-6 col-lg-4 " style="padding-top: 10px;">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-3">
+                            <img class="picture"
+                                src="https://parisnail.ru/upload/resize_cache/iblock/352/162_162_1/2b30cc07_0ff6_11e8_81b9_0cc47a49d0fc_702258c9_5443_11e8_a667_00155d010201.jpg">
+                        </div>
+                        <div class="col-9">
+                            <div class="text">Гель-лак OGP-041 Nightshade ONIQ 10 мл...</div>
+                            <div class="row">
+                                <div class="col-8">
+                                    <div class="text-price" style="display:inline;">1 050P</div>
+                                    <div class="text-price-old" style="display:inline;"> 2 000P </div>
+                                </div>
+    
+                                <div class="col-4 svg-cart">
+    
+                                    <img src="cart2.svg">
+    
+                                </div>
+                            </div>
+                        </div>
+    
+                    </div>
+    
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-6 col-lg-4 " style="padding-top: 10px;">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-3">
+                            <img class="picture"
+                                src="https://parisnail.ru/upload/resize_cache/iblock/352/162_162_1/2b30cc07_0ff6_11e8_81b9_0cc47a49d0fc_702258c9_5443_11e8_a667_00155d010201.jpg">
+                        </div>
+                        <div class="col-9">
+                            <div class="text">Гель-лак OGP-041 Nightshade ONIQ 10 мл...</div>
+                            <div class="row">
+                                <div class="col-8">
+                                    <div class="text-price" style="display:inline;">1 050P</div>
+                                    <div class="text-price-old" style="display:inline;"> 2 000P </div>
+                                </div>
+    
+                                <div class="col-4 svg-cart">
+    
+                                    <img src="cart2.svg">
+    
+                                </div>
+                            </div>
+                        </div>
+    
+                    </div>
+    
+                </div>
+            </div>
+        </div>
+<!-- КОНЕЦ ПЕРВОЙ ПОЛОСЫ-->
+
+       
+
+<div style="text-align: center; padding:20px">
+
+    <button class="btn-result"> Посмотреть все результаты (40) </button>
+</div>
+
+</div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+        @font-face {
+            font-family: 'Futura New';
+            src: local('Futura New Book Reg'), local('Futura-New-Book-Reg'),
+                url('FuturaNewBook-Reg.woff2') format('woff2'),
+                url('FuturaNewBook-Reg.woff') format('woff'),
+                url('FuturaNewBook-Reg.ttf') format('truetype');
+            font-weight: normal;
+            font-style: normal;
+        }
+
+        @font-face {
+            font-family: 'Futura New Demi';
+            src: local('Futura New Book Reg'), local('Futura-New-Book-Reg'),
+                url('FuturaNewBook-Reg.woff2') format('woff2'),
+                url('FuturaNewBook-Reg.woff') format('woff'),
+                url('FuturaNewBook-Reg.ttf') format('truetype');
+            font-weight: Demi;
+            font-style: bold;
+        }
+
+
+
+        .card {
+            width: 315px;
+            height: 90px;
+            background: #FFFFFF 0% 0% no-repeat padding-box;
+            border-radius: 10px;
+            padding: 10px;
+            transition:0.2s;
+        }
+
+        .card:hover {
+            width: 315px;
+            height: 90px;
+            background: #FFDFDA 0% 0% no-repeat padding-box;
+            border-radius: 10px;
+            padding: 10px;
+            transition: 0.2s;
+
+        }
+
+        .card:hover .text-price-old {
+            color: black;
+        }
+
+        .card:hover .svg-cart {
+            opacity: 0.6;
+        }
+
+        .picture {
+
+            width: 70px;
+            height: 70px;
+            border-radius: 10px;
+            opacity: 1;
+        }
+
+        .text {
+            text-align: left;
+            font: normal normal normal 16px/22px Futura New;
+            letter-spacing: 0px;
+            color: #191919;
+            opacity: 1;
+        }
+
+        .text-price {
+            text-align: left;
+            font: normal normal normal 16px/10px Futura New;
+            letter-spacing: 0px;
+            color: #191919;
+            opacity: 1;
+        }
+
+        .text-price-old {
+            text-align: left;
+            text-decoration: line-through;
+            font: normal normal normal 12px/10px Futura New;
+            letter-spacing: 0px;
+            color: #B1B1B1;
+            opacity: 1;
+        }
+
+        .svg-cart {
+            text-align: right;
+            opacity: 0.3;
+        }
+
+        .svg-cart:hover {
+            text-align: right;
+            opacity: 0.6;
+        }
+
+        .body {
+            width:996px;
+            
+            background: #F5F5F5 0% 0% no-repeat padding-box;
+            border-radius: 0px 0px 15px 15px;
+            opacity: 1;
+            border: 1px solid;
+            padding: 10px;
+            overflow-x: hidden;
+        }
+
+        .padding {
+            padding-bottom:0px;
+            padding-left:10px;
+            padding-right:10px;
+            padding-top: 10px;
+        }
+
+        .btn-result {
+            width: 315px;
+            height: 36px;
+            background: #FFDFDA 0% 0% no-repeat padding-box;
+            border-radius: 7px;
+            border: none;
+            transition:0.2s;
+            
+        }
+        .btn-result:hover {
+            background: #FF527D 0% 0% no-repeat padding-box;
+            color:white;
+            transition:0.2s;
+        }
+
+        .header-block-search {
+            font: bold 18px Futura New ;
+            padding:10px;
+        }
+
+        .chip {
+            
+            
+            height: 29px;
+            border: 1px solid #FFCAC8;
+            border-radius: 30px;
+            padding-left:10px;
+            padding-right:10px;
+            margin-bottom: 10px;
+            margin-left: 10px;
+            display:inline-block;
+        }
+
+        .chip:hover {
+            
+            border: 1px solid #FF527D;
+            border-radius: 30px;
+ 
+        }
+
+        .result-label{
+            text-align: left;
+            font: normal normal normal 16px/10px Futura New;
+            letter-spacing: 0px;
+            color: #000000;
+            opacity: 1;
+            
+            display:inline;
+        }
+
+        .result-label-search{
+            text-align: left;
+            font: oblique normal normal 16px/10px Futura New;
+            letter-spacing: 0px;
+            color: #FFA39F;
+            display:inline;
+        }
+
+        .image {
+            background-image:url('cart2.svg') ;
+            width: 16px;
+            height: 20px;
+            float: right;
+        }
+        @media (min-width: 768px ) and (max-width: 996px)   {
+            .body {width: 670px};
+        }
+        @media (min-width: 0px ) and (max-width: 767px)   {
+            .body {width: 350px};
+        }
+</style>
